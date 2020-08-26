1c96a0f8625f12edcdf86a3e28fae85b024fb2aa
xander <1390838101@qq.com>
2020-08-26 22:13:50 +0800

    feat(account.go): add account struct

---
+++ account.go
@@ -0,0 +1,13 @@
+package asi
+
+import (
+	"github.com/jinzhu/gorm"
+)
+
+type Account struct {
+	gorm.Model
+	Email    string `gorm:"type:varchar(100);unique_index"json:"email"`
+	Username string `gorm:"type:varchar(100);unique_index"json:"username"`
+	Password string `gorm:"column:encrypted_password" json:"-"`
+	Token    string `gorm:"-" json:"token"`
+}
